646f6c5405a613333e811a3b32ffcf900ace980e
Romas <r2udis@gmail.com>
2026-04-03 22:00:41 +0300

    Add page for DB seed

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,41 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  text,
+  index,
+  singlestoreTableCreator,
+  bigint,
+} from "drizzle-orm/singlestore-core";
 
-export const user = singlestoreTable("user_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+  (name) => `drive-t3_${name}`,
+);
+
+export const files_table = createTable(
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders_table = createTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
